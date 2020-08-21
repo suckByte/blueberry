b565225113e46d0e3830fe3505dfa0d9702db3ac
suckByte <3.1415bemyself@gmail.com>
2020-08-21 17:39:35 +0800

    sth change

--- src/main/java/com/blueberry/mytest/Generator.java
+++ src/main/java/com/blueberry/mytest/Generator.java
@@ -1,17 +1,15 @@
 package com.blueberry.mytest;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.mybatis.generator.api.MyBatisGenerator;
 import org.mybatis.generator.config.Configuration;
 import org.mybatis.generator.config.xml.ConfigurationParser;
-import org.mybatis.generator.exception.XMLParserException;
 import org.mybatis.generator.internal.DefaultShellCallback;
 
-public class MBGTest {
+public class Generator {
 
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
